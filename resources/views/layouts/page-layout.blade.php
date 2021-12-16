d734cc4b54f1114839175ea089b62b3e9ce827c3
Hidalgo Ponce <hidalgoponce@Hidalgos-MacBook-Pro.local>
2021-12-15 21:20:06 -0500

    whatsapp update

--- resources/views/layouts/page-layout.blade.php
+++ resources/views/layouts/page-layout.blade.php
@@ -270,14 +270,14 @@
 
 
     <div class="fixed bottom-0 right-0 z-40 py-6 px-7 mb-24 sm:mb-0 sm:inline-flex">
-        <a href="https://api.whatsapp.com/send?phone=51960106076" target="_blank">
+        <a href="https://api.whatsapp.com/send?phone=12024911478" target="_blank">
             <span class="flex relative h-10 w-10">
               <span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-gray-400 opacity-75"></span>
             </span>
         </a>
     </div>
     <div class="fixed bottom-0 right-0 z-40 py-5 px-4 mb-24 sm:mb-0 sm:inline-flex">
-        <a href="https://api.whatsapp.com/send?phone=51960106076" target="_blank">
+        <a href="https://api.whatsapp.com/send?phone=12024911478" target="_blank">
             <img src="{{asset('images/whatsapp-i.png')}}" alt="" class="w-16">
         </a>
     </div>
