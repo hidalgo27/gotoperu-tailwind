7155fcfd463773ed1f2fe0c891d08192c0191dd4
Hidalgo Ponce <hidalgoponce@Hidalgos-MacBook-Pro.local>
2022-01-02 22:33:01 -0500

    Se actualizo amplitudw

--- resources/views/components/packages-card.blade.php
+++ resources/views/components/packages-card.blade.php
@@ -34,7 +34,7 @@
 
         </div>
         @if($paquete['codigo_f'])
-            <button class="wtrvl-checkout_button btn-primary block w-full mb-2" id="wetravel_button_widget" data-env="https://www.wetravel.com" data-version="v0.2" data-uid="239346" data-uuid="{{$paquete['codigo_f']}}" href="https://www.wetravel.com/checkout_embed?uuid={{$paquete['codigo_f']}}" >{{__('message.button_book')}}</button>
+            <button class="wtrvl-checkout_button btn-primary block w-full mb-2" id="wetravel_button_widget" data-env="https://www.wetravel.com" data-version="v0.2" data-uid="{{$paquete['codigo_f']}}" data-uuid="{{$paquete['codigo_f']}}" href="https://www.wetravel.com/checkout_embed?uuid={{$paquete['codigo_f']}}" >{{__('message.button_book')}}</button>
         @endif
         <a href="{{route('packages.detail', $paquete['url'])}}" class="btn-secondary block">{{__('message.button_detail')}}</a>
     </div>
