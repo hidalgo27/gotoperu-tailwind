d2d38e572afb95cbccf3551e0d5ecb6b61591fd5
Hidalgo Ponce <hidalgoponce@Hidalgos-MacBook-Pro.local>
2022-05-03 15:53:38 -0500

    Prices

--- resources/views/page/category-show.blade.php
+++ resources/views/page/category-show.blade.php
@@ -56,6 +56,10 @@
                             @endif
                         @endforeach
 
+                        @if($paquete['codigo_f'])
+                            <button class="wtrvl-checkout_button btn-primary block w-full mb-2" id="wetravel_button_widget" data-env="https://www.wetravel.com" data-version="v0.2" data-uid="239346" data-uuid="{{$paquete['codigo_f']}}" href="https://www.wetravel.com/checkout_embed?uuid={{$paquete['codigo_f']}}" >{{__('message.button_book')}}</button>
+                        @endif
+
                     </div>
                     <a href="{{route('packages.detail', $paquete['paquetes']['url'])}}" class="btn-secondary">{{__('message.button_detail')}}</a>
                 </div>
