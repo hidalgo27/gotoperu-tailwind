1d2d8dd89c778888a4b5a5f302bff84a38544304
Hidalgo Ponce <hidalgoponce@Hidalgos-MacBook-Pro.local>
2022-07-14 21:49:40 -0500

    Hotjar and olark update

--- resources/views/layouts/page-layout.blade.php
+++ resources/views/layouts/page-layout.blade.php
@@ -49,6 +49,17 @@
 
 
     </script>
+    <!-- Hotjar Tracking Code for https://gotoperu.com -->
+    <script>
+        (function(h,o,t,j,a,r){
+            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+            h._hjSettings={hjid:3067283,hjsv:6};
+            a=o.getElementsByTagName('head')[0];
+            r=o.createElement('script');r.async=1;
+            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+            a.appendChild(r);
+        })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+    </script>
 
     <style>
         .preload {
@@ -662,6 +673,20 @@
 <script src="{{asset('vendor/intl-tel-input/build/js/utils.js')}}"></script>
 <script src="{{asset('js/plugins.js')}}"></script>
 @stack('scripts')
+<!-- begin olark code -->
+<script type="text/javascript">
+    ;(function(o,l,a,r,k,y){if(o.olark)return;
+        r="script";y=l.createElement(r);r=l.getElementsByTagName(r)[0];
+        y.async=1;y.src="//"+a;r.parentNode.insertBefore(y,r);
+        y=o.olark=function(){k.s.push(arguments);k.t.push(+new Date)};
+        y.extend=function(i,j){y("extend",i,j)};
+        y.identify=function(i){y("identify",k.i=i)};
+        y.configure=function(i,j){y("configure",i,j);k.c[i]=j};
+        k=y._={s:[],t:[+new Date],c:{},l:a};
+    })(window,document,"static.olark.com/jsclient/loader.js");
+    /* Add configuration calls below this comment */
+    olark.identify('2217-527-10-8230');</script>
+<!-- end olark code -->
 <script>
     (() => {
         'use strict';
