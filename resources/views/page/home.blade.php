9e1bafbb23ad141aa954ae2e697520dbe4b7bca8
Hidalgo Edely Chata Ponce <hidalgochponce@gmail.com>
2023-11-13 22:28:39 -0500

    Campaña black friday

--- resources/views/page/home.blade.php
+++ resources/views/page/home.blade.php
@@ -976,6 +976,51 @@
         </div>
     </section>
 
+
+    <div x-data="{ open: false, init() { setTimeout(() => this.open = true, 3000) } }" x-init="init()">
+{{--        <button @click="open =!open" class="flex items-center justify-center px-3 py-2 space-x-2 text-sm tracking-wide  capitalize transition-colors duration-200 transform bg-indigo-500 rounded-md dark:bg-indigo-600 dark:hover:bg-indigo-700 dark:focus:bg-indigo-700 hover:bg-indigo-600 focus:outline-none focus:bg-indigo-500 focus:ring focus:ring-indigo-300 focus:ring-opacity-50">--}}
+{{--            <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" viewBox="0 0 20 20" fill="currentColor">--}}
+{{--                <path fill-rule="evenodd" d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z" clip-rule="evenodd" />--}}
+{{--            </svg>--}}
+
+{{--            <span>Invite Member</span>--}}
+{{--        </button>--}}
+{{--        ssf--}}
+
+        <div x-show="open" class="fixed inset-0 z-50 overflow-y-auto" aria-labelledby="modal-title" role="dialog" aria-modal="true">
+            <div class="flex items-end justify-center min-h-screen px-4 text-center md:items-center sm:block sm:p-0">
+                <div x-cloak @click="open = false" x-show="open"
+                     x-transition:enter="transition ease-out duration-300 transform"
+                     x-transition:enter-start="opacity-0"
+                     x-transition:enter-end="opacity-100"
+                     x-transition:leave="transition ease-in duration-200 transform"
+                     x-transition:leave-start="opacity-100"
+                     x-transition:leave-end="opacity-0"
+                     class="fixed inset-0 transition-opacity bg-gray-500 bg-opacity-40" aria-hidden="true"
+                ></div>
+
+                <div x-cloak x-show="open"
+                     x-transition:enter="transition ease-out duration-300 transform"
+                     x-transition:enter-start="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                     x-transition:enter-end="opacity-100 translate-y-0 sm:scale-100"
+                     x-transition:leave="transition ease-in duration-200 transform"
+                     x-transition:leave-start="opacity-100 translate-y-0 sm:scale-100"
+                     x-transition:leave-end="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                     class="inline-block w-full relative max-w-2xl my-40 overflow-hidden text-left transition-all transform bg-white rounded-lg shadow-xl 2xl:max-w-3xl"
+                >
+
+                        <button @click="open = false" class="text-gray-600 focus:outline-none absolute top-0 right-0 p-3 hover:text-gray-700">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                            </svg>
+                        </button>
+
+                    <a href="https://gotolatam.com/peru-travel-packages/Brazil-Argentina-Peru-22-days"><img src="{{('images/banners/campaigns/black.jpg')}}" alt=""></a>
+                </div>
+            </div>
+        </div>
+    </div>
+
     <section class="mt-12">
         @livewire('page.form-footer')
     </section>
@@ -1025,6 +1070,15 @@
             },
         });
     </script>
+
+{{--    <script>--}}
+{{--        document.addEventListener("DOMContentLoaded", function() {--}}
+{{--            setTimeout(() => {--}}
+{{--                document.querySelector('[x-data]').__x.$data.open = true;--}}
+{{--            }, 2000); // 10000 milisegundos = 10 segundos--}}
+{{--        });--}}
+{{--    </script>--}}
+
 
 @endpush
 
