fecd0485c236050e7dcaeb4e2588df6289572d2d
Hidalgo Edely Chata Ponce <hidalgochponce@gmail.com>
2025-01-19 10:40:17 -0500

    correos actualizados

--- resources/views/layouts/page-layout.blade.php
+++ resources/views/layouts/page-layout.blade.php
@@ -318,14 +318,14 @@
 </div>
 
     <div class="fixed bottom-0 mb-20 right-0 z-40 py-6 px-7 sm:inline-flex md:mb-0">
-        <a href="https://api.whatsapp.com/send?phone=51936241194" target="_blank">
+        <a href="https://api.whatsapp.com/send?phone=51934991442" target="_blank">
             <span class="flex relative h-10 w-10">
               <span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-gray-400 opacity-75"></span>
             </span>
         </a>
     </div>
     <div class="fixed bottom-0 mb-20 right-0 z-40 py-5 px-4 sm:inline-flex md:mb-0">
-        <a href="https://api.whatsapp.com/send?phone=51936241194" target="_blank">
+        <a href="https://api.whatsapp.com/send?phone=51934991442" target="_blank">
             <img src="{{asset('images/whatsapp-i.png')}}" alt="" class="w-16">
         </a>
     </div>
