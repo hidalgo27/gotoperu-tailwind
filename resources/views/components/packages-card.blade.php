23d592493a2b72fae91a7e8b67f47b88f5020ce2
Hidalgo Edely Chata Ponce <hidalgochponce@gmail.com>
2025-08-27 18:57:14 -0500

    refactor: datail page

--- resources/views/components/packages-card.blade.php
+++ resources/views/components/packages-card.blade.php
@@ -2,8 +2,11 @@
 
 <div  class="group   border hover:border-primary transition duration-500 hover:shadow-xl ">
     <div class="overflow-hidden relative">
+        <a href="{{ route('packages.detail', $paquete) }}">
         <div class="relative z-0">
-            <img src="{{ $paquete->imagen }}" alt="{{ $paquete['titulo'] }}" class="object-cover h-96 w-full transition duration-500 ease-in-out transform group-hover:-translate-x-0 group-hover:scale-105"/>
+
+                <img src="{{ $paquete->imagen }}" alt="{{ $paquete['titulo'] }}" class="object-cover h-96 w-full transition duration-500 ease-in-out transform group-hover:-translate-x-0 group-hover:scale-105"/>
+
             <div class="absolute inset-0 gradient-cicle-gray z-0"></div>
             <div class="absolute top-0 left-0 p-4 z-50 flex flex-wrap gap-1">
 
@@ -15,6 +18,7 @@
                 @endforeach
             </div>
         </div>
+        </a>
 
 {{--        <div class="absolute inset-x-0 bottom-0 w-full p-6">--}}
 {{--            <div class="bg-primary bg-opacity-95 p-4 text-gray-50 group-hover:bg-opacity-100 transition duration-500 rounded-lg shadow-xl">--}}
