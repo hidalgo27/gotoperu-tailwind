7c10e3400183045c580fc6d3694ddadd0a1aac24
Hidalgo Edely Chata Ponce <hidalgochponce@gmail.com>
2025-09-16 21:18:57 -0500

    refactor: datail page

--- resources/views/layouts/page-layout.blade.php
+++ resources/views/layouts/page-layout.blade.php
@@ -433,7 +433,8 @@
 
 {{--                @livewire('page.form-inquire')--}}
 {{--                @livewire('page.form-subscribe')--}}
-                <a href="#form-dream-adventure" class="btn-dark text-center">{{__('message.button_inquire')}}</a>
+                <span>info@gotoperu.com</span>
+                <a href="#form-dream-adventure" class="btn-secondary text-center">{{__('message.button_inquire')}}</a>
                 <div class="dark:text-gray-400">
                     <span class="text-xs block text-right">US</span>
                     +1 (813) 455-8646
